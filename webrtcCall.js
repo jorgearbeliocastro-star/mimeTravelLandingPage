5089624dae412e026ceaf515513c3a1d6019e7bf
Jorge Arbelio Castro <jorgearbeliocastro@gmail.com>
2026-08-26 20:23:20 -0400

    Auto-commit: update webrtcCall.js

--- webrtcCall.js
+++ webrtcCall.js
@@ -57,9 +57,11 @@ function startWebRTCCall({ supabaseClient, channelName, isCaller, localVideoEl, 
   }
 
   (async function start() {
+    console.log('[call:' + (isCaller ? 'caller' : 'callee') + ']', 'iniciando, canal:', channelName);
     try {
       localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
     } catch (e) {
+      console.log('[call]', 'getUserMedia falló:', e.name, e.message);
       onState('failed');
       return;
     }
